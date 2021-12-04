b1b168a71b80cd65ce02e33a397c99276655a928
Chris <chris.feight@gmail.com>
2021-12-03 21:53:53 -0800

    introducing components

---
+++ src/components/homeDisplay.js
@@ -0,0 +1,18 @@
+import '../App.css';
+
+function HomeDisplay() {
+    return (
+        <div className="App">
+            <h1>
+                Originally from California, I have lived in Seattle for the past 7 years. My previous work
+                experience has been quite varied, and I enjoy finding new and challenging opportunities to utilize
+                critical thinking, implement collaborative processes, and continue to seek environments where I can
+                grow personally and professionally. One of my favorite hobbies is international travel, and I look
+                forward to the day where I can combine my passion for technology and travel into a rewarding and
+                enriching career.
+            </h1>
+        </div>
+    );
+}
+
+export default HomeDisplay;
---
+++ src/components/navBar.js
@@ -0,0 +1,17 @@
+import '../App.css';
+
+function Navbar() {
+    return (
+        <div className="navbar">
+            <div className="navname"><a href="#">Christopher Ochota-Feight</a></div>
+            <div className="navlinks">
+                <a href="#" >Resume</a>
+                <a href="#" >About Me</a>
+                <a href="#" >Work</a>
+                <a href="#" >Contacts</a>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
